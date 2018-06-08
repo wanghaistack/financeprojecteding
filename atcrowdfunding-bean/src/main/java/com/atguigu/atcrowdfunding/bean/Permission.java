4c34edee2bcd260a5a282529d33aa19c065d880d
wanghai <451299586@qq.com>
2018-06-08 23:43:04 +0800

    activiti流程服务 bean

---
+++ atcrowdfunding-bean/src/main/java/com/atguigu/atcrowdfunding/bean/Permission.java
@@ -0,0 +1,88 @@
+package com.atguigu.atcrowdfunding.bean;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Permission {
+    private Integer id;
+
+    private Integer pid;
+
+    private String name;
+
+    private String icon;
+
+    private String url;
+    
+    private Boolean open = true;
+    
+    private List<Permission> children =new ArrayList<Permission>();
+    
+    private boolean checked;
+    
+
+    public boolean isChecked() {
+		return checked;
+	}
+
+	public void setChecked(boolean checked) {
+		this.checked = checked;
+	}
+
+	public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getPid() {
+        return pid;
+    }
+
+    public void setPid(Integer pid) {
+        this.pid = pid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+
+    public String getIcon() {
+        return icon;
+    }
+
+    public void setIcon(String icon) {
+        this.icon = icon == null ? null : icon.trim();
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url == null ? null : url.trim();
+    }
+
+	public Boolean getOpen() {
+		return open;
+	}
+
+	public void setOpen(Boolean open) {
+		this.open = open;
+	}
+
+	public List<Permission> getChildren() {
+		return children;
+	}
+
+	public void setChildren(List<Permission> children) {
+		this.children = children;
+	}
+    
+}
